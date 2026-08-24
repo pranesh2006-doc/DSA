3c9ebbf8ef7eebf6a16528c98c4ee5821c71d7ce
pranesh2006-doc <praneshdevaraj0@gmail.com>
2026-08-24 22:04:48 +0530

    leedcode low lvl

---
+++ TwoSum.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class TwoSum {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter target: ");
+        int target = sc.nextInt();
+
+        boolean found = false;
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = i + 1; j < n; j++) {
+
+                if (arr[i] + arr[j] == target) {
+                    System.out.println(
+                        "Pair found: " + arr[i] + " + " + arr[j] + " = " + target
+                    );
+                    found = true;
+                }
+            }
+        }
+
+        if (!found) {
+            System.out.println("No pair found");
+        }
+
+        sc.close();
+    }
+}
